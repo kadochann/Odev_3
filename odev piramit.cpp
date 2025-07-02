0632645fd2b014763132ad896481c3b94c71c457
Taha İçöz <tahaicoz26@gmail.com>
2025-07-02 20:12:23 +0300

    odevs

---
+++ odev piramit.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int istenilenY;
+
+    cout << "ucgenin yuksekligi kac olsun : ";
+    cin >> istenilenY;  
+
+    for (int line = 1; line <= istenilenY; line++) {
+
+        for (int bosluk = 1; bosluk <= (istenilenY - line); bosluk++) {
+            cout << " ";
+        }
+
+        for (int yildiz = 1; yildiz <= (2 * line - 1); yildiz++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
